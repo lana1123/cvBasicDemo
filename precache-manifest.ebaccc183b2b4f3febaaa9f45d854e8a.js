4f1c52fad144e8c1ccebfb72df4f8f75d054aa37
lana wong <lana_1123@hotmail.com>
2020-06-20 07:54:07 +0800

    Updates

---
+++ precache-manifest.ebaccc183b2b4f3febaaa9f45d854e8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7fd1b94a1c9e1b9f1916d5a8b2db64d",
+    "url": "/cvBasicDemo/index.html"
+  },
+  {
+    "revision": "6b235ec1bbfbab9f621b",
+    "url": "/cvBasicDemo/static/css/main.77cdbd8e.chunk.css"
+  },
+  {
+    "revision": "4f8574293cc01a44a851",
+    "url": "/cvBasicDemo/static/js/2.ae3cd34c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cvBasicDemo/static/js/2.ae3cd34c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b235ec1bbfbab9f621b",
+    "url": "/cvBasicDemo/static/js/main.93507fa5.chunk.js"
+  },
+  {
+    "revision": "d6c5b650cccbda156642",
+    "url": "/cvBasicDemo/static/js/runtime-main.7c1571f9.js"
+  },
+  {
+    "revision": "d47ee162ea813b46ccdef5e541ac5caf",
+    "url": "/cvBasicDemo/static/media/ProfilePicture.d47ee162.JPG"
+  }
+]);
